ae708e57fcd3388bf15bd8a3f08c1171b551ef30
mkalluri92 <mkalluri92@gmail.com>
2020-04-23 04:52:40 -0700

    added for mobile version

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => {
-    return <Aux>
-        <Toolbar />
-        <SideDrawer />
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+
+    menuClicked = (prevState) => {
+        return (this.setState({
+            showSideDrawer: !this.state.showSideDrawer
+        }))
+    }
+
+    render () {
+        return <Aux>
+        <Toolbar toggleClicked={this.menuClicked}/>
+        <SideDrawer 
+            open={this.state.showSideDrawer}
+            closed={this.sideDrawerCloseHandler}/>
         <div>Toolbar, sidebar, backdrop</div>
         <div className = {classes.content}>
-            {props.children}
+            {this.props.children}
         </div>
     </Aux>
+    }
+    
 }
 
 export default Layout;
