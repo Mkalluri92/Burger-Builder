44ea1b5b310341d08dcc73d5a2958c4641bc7baf
mkalluri92 <mkalluri92@gmail.com>
2020-05-19 16:58:41 -0700

    getting orders from server using redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -35,6 +35,23 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 loading: false
             }
+        case actionTypes.FETCH_ORDERS_START:
+            return {
+                ...state,
+                loading: true
+            }
+        case actionTypes.FETCH_ORDERS_SUCCESS:
+            console.log(state);
+            return {
+                ...state,
+                orders: action.orders,
+                loading: false
+            }
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false
+            }
         default:
             return state
     }
