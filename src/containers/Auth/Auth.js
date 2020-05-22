e2776dd2fdb62d7a4677d24c72950779be16fa4e
mkalluri92 <mkalluri92@gmail.com>
2020-05-22 04:17:12 -0700

    added validations to authentication page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -39,6 +39,52 @@ class Auth extends Component {
         }
     }
 
+    checkValidity(value, rules) {
+        let _isValid = true;
+
+        if(!rules) {
+            return true;
+        }
+
+        if(rules.required) {
+            _isValid = value.trim() !== '' && _isValid;
+        }
+
+
+        if(rules.minLength) {
+            _isValid = value.length >= rules.minLength && _isValid;
+        }
+
+        if(rules.maxLength) {
+            _isValid = value.length <= rules.maxLength && _isValid;
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            _isValid = pattern.test(value) && _isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            _isValid = pattern.test(value) && _isValid
+        }
+
+        return _isValid
+    }
+
+    inputChangeHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls})
+    }
+
     render () {
         let formElemenetsArray = [];
             for (let key in this.state.controls) {
